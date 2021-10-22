18df78052d546399e604cf2fe239a3dcfb8609b1
Jakob Danilo <danilo.jakob@wuerth-itensis.com>
2021-10-22 15:36:24 +0200

    ongoing development: establish ssh connection

--- main.py
+++ main.py
@@ -2,7 +2,41 @@ import requests
 import time
 from stem import Signal
 from stem.control import Controller
+import os
+import pwd
+import socket
+import socks
+from ssh.session import Session
+from ssh import options
 
+
+def connect_to_ssh() : 
+    # Linux only
+    USERNAME = "jada"
+    HOST = '192.168.56.101'
+    PORT = 22
+
+    sock = socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150, True)
+    sock.connect((HOST, PORT))
+
+    s = Session()
+    s.options_set(options.HOST, HOST)
+    s.options_set(options.USER, USERNAME)
+    s.options_set_port(PORT)
+    s.set_socket(sock)
+    s.connect()
+
+    # Authenticate with agent
+    s.userauth_agent(USERNAME)
+
+    chan = s.channel_new()
+    chan.open_session()
+    chan.request_exec('echo me')
+    size, data = chan.read()
+    while size > 0:
+        print(data.strip())
+        size, data = chan.read()
+    chan.close()
 
 def get_current_ip():
     session = requests.session()
